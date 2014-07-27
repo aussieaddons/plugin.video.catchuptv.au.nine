1f1315ebc8a17028680cfed44e24280cd918b827
Adam Malcontenti-Wilson <adman.com@gmail.com>
2014-07-27 22:29:06 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+Ninemsn Jump In plugin for XBMC
+===============================
+
+This plugin allows you to watch videos from Ninemsn's Jump In in XBMC.
+
+The content is only available within Australia from an Australian IP address.
+
+Installation
+------------
+The plugin is in beta and not yet available within the repository, instead
+download [GitHub generated ZIP file] [githubzip] to your XBMC device and 
+install it through the menu 
+via System -> Settings -> Add-ons -> Install from zip file.
+
+Issues
+------
+For any issues or bug reports, please file them on the [issues page] [issues].
+
+Please include log output if possible, using [Github Gist] [gist] or 
+[Pastebin.com] [pastebin].
+
+The location of your XBMC log file will depend on your platform, 
+see the [XBMC Wiki page] [xbmc-wiki-log-file] for information.
+
+[githubzip]: https://github.com/xbmc-catchuptv-au/plugin.video.catchuptv.au.nine/archive/master.zip
+[issues]: https://github.com/xbmc-catchuptv-au/plugin.video.catchuptv.au.nine/issues
+[gist]: https://gist.github.com
+[pastebin]: http://pastebin.com/
+[xbmc-wiki-log-file]: http://wiki.xbmc.org/index.php?title=Log_file/Advanced#Log_files
---
+++ default.py
@@ -0,0 +1,112 @@
+from brightcove.api import Brightcove
+from xbmcswift2 import Plugin, SortMethod
+from urllib import urlopen, quote
+import json
+
+JUMPIN_WIDGET_CONFIG_URL = 'http://jumpin-widgets.static9.net.au/globalconfig/iphone'
+TV_CAT_API_URL = 'http://tv-api-cat.api.jump-in.com.au'
+
+plugin = Plugin()
+
+def load_json(url):
+  fp = urlopen(url)
+  return json.load(fp)
+
+def filter_drm_shows(show):
+  return not show['drm']
+
+def filter_has_episodes(show):
+  return show['episodeCount'] > 0
+
+def map_tv_channel(channel):
+  if channel == 'Channel 9':
+    return 'Nine Network'
+  else:
+    return channel
+
+def show_data_to_xbmc_dict(show):
+  return {
+    'label': show['title'],
+    'thumbnail': show['image']['showImage'],
+    'path': plugin.url_for('show', slug=show['slug']),
+    'info': {
+      'plot': show['description'],
+      'plotoutline': show['description'],
+      'tvshowtitle': show['title'],
+      'studio': map_tv_channel(show['tvChannel']),
+      'genre': show['genre']
+    },
+    'properties': {
+      'fanart_image': show['image']['showImage']
+    }
+  }
+
+def season_data_to_xbmc_dict(season):
+  return {
+    'label': season['title'],
+    'path': plugin.url_for('season', slug=season['slug'])
+  }
+
+def episode_data_to_xbmc_dict(episode):
+  return {
+    'label': episode['title'],
+    'thumbnail': episode['images']['videoStill'],
+    'path': plugin.url_for('play', videoId=episode['videoId']),
+    'info': {
+      'plot': episode['description'],
+      'plotoutline': episode['description'],
+      'episode': episode['episodeNumber'],
+      'genre': episode['genre'],
+      'studio': map_tv_channel(episode['tvChannel'])
+    },
+    'is_playable': True
+  }
+
+def get_api_token(type = 'ninemsnCatchup'):
+  data = load_json(JUMPIN_WIDGET_CONFIG_URL)
+  return data['brightcoveApiTokenLookup'][type]
+
+@plugin.route('/')
+def index():
+  data = load_json(TV_CAT_API_URL + '/shows?take=200')
+  shows = filter(filter_has_episodes, filter(filter_drm_shows, data['payload']))
+  items = map(show_data_to_xbmc_dict, shows)
+  plugin.set_content('tvshows')
+  return plugin.finish(items, sort_methods=[SortMethod.LABEL_IGNORE_THE])
+
+@plugin.route('/shows/<slug>')
+def show(slug):
+  data = load_json(TV_CAT_API_URL + '/shows/' + quote(slug, safe='') + '?fields=true')
+
+  items = map(season_data_to_xbmc_dict, data['seasons'])
+  plugin.set_content('seasons')
+  return plugin.finish(items)
+
+@plugin.route('/seasons/<slug>')
+def season(slug):
+  data = load_json(TV_CAT_API_URL + '/seasons/' + quote(slug, safe='') + '?fields=true')
+
+  items = map(episode_data_to_xbmc_dict, data['episodes'])
+
+  def add_show_data(item):
+    item['info'].update({
+      "season": data['title'],
+    })
+    item['properties'] = {
+      'fanart_image': data['show']['image']['showImage']
+    }
+    return item
+  items = map(add_show_data, items)
+
+  plugin.set_content('episodes')
+  return plugin.finish(items)
+
+@plugin.route('/play/<videoId>')
+def play(videoId):
+  api_token = get_api_token()
+  brightcove = Brightcove(api_token)
+  video = brightcove.find_video_by_id(videoId, media_delivery='http')
+  plugin.set_resolved_url(video.videoFullLength['url'])
+
+if __name__ == '__main__':
+    plugin.run()
