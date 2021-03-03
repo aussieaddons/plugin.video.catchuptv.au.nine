ee1c746d9bbd8c205c4fa9d4c46e4e7966639867
Glenn Guy <glennguy83@gmail.com>
2021-03-03 16:49:59 +1100

    make parse_kodi_url safe

--- resources/lib/classes.py
+++ resources/lib/classes.py
@@ -51,7 +51,7 @@ class BaseItem(object):
 
     def parse_kodi_url(self, url):
         params = dict(parse_qsl(url))
-        params.pop('addon_version')
+        params.pop('addon_version', '')
         for item in params.keys():
             setattr(self, item, unquote_plus(params[item]))
 
